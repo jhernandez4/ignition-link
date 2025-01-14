47c36ca11ed90db88b5e18b4bce28556ed37deef
jhernandez4 <jokhernandez120744@gmail.com>
2025-01-13 18:23:07 -0800

    Initialize Firebase SDK

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials
+from dotenv import load_dotenv
+import os
+
+load_dotenv
+
+FIREBASE_KEY_PATH = os.environ.get("FIREBASE_KEY_PATH")
+cred = credentials.Certificate(FIREBASE_KEY_PATH)
+firebase_admin.initialize_app(cred)
 
 app = FastAPI()
 

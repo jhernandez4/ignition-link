2026458c06c0bb213d81e05a19cf138906ab25e6
jhernandez4 <jokhernandez120744@gmail.com>
2025-01-16 23:37:02 -0800

    Remove firebase functions from main file

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from fastapi import FastAPI
-import firebase_admin
-from firebase_admin import credentials
-from dotenv import load_dotenv
-import os
-
 from database import User, create_db_and_tables, SessionDep
-
-load_dotenv()
-
-FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY_PATH")
-cred = credentials.Certificate(FIREBASE_KEY_PATH)
-firebase_admin.initialize_app(cred)
 
 app = FastAPI()
 

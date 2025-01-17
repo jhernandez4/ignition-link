01de5fdf0697e593746285ed13ff87f644311c5a
jhernandez4 <jokhernandez120744@gmail.com>
2025-01-16 23:39:05 -0800

    Connect auth router to main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from database import User, create_db_and_tables, SessionDep
+from .routers import auth
 
 app = FastAPI()
+
+app.include_router(auth.router)
 
 @app.on_event("startup")
 def on_startup():

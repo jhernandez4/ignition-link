c0635db4d43bb3099716188ac5fa05481f49a2f2
jhernandez4 <jokhernandez120744@gmail.com>
2025-02-10 19:31:35 -0800

    fix: allow cross origin requests
    Additionally, this change specifies the frontend origin so that it is
    allowed to make calls the backend.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import User, create_db_and_tables, SessionDep
 from .routers import auth, validation
 import firebase_admin
@@ -7,6 +8,19 @@ from dotenv import load_dotenv
 import os
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(auth.router)
 app.include_router(validation.router)

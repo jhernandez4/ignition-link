3bca5cc7ad611b45bde8c5e9bb1ee27caf89907a
jhernandez4 <jokhernandez120744@gmail.com>
2025-02-17 17:04:32 -0800

    fix: add deployed frontend to cors policy

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://ignitionlink-frontend.vercel.app/login"
 ]
 
 app.add_middleware(

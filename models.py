e9b9f43b7d087f8cdb7698a4fe51c5a55f44c2e3
jhernandez4 <jokhernandez120744@gmail.com>
2025-03-16 22:55:49 -0700

    chore: response model for builds endpoints

--- models.py
+++ models.py
@@ -11,3 +11,24 @@ class UserResponse(BaseModel):
     class Config:
         # Enable support for ORM mode
         from_attributes = True
+
+class VehicleResponse(BaseModel):
+    id: int
+    make: str
+    model: str
+    year: int
+
+    class Config:
+        orm_mode = True  # This allows the Pydantic model to work with SQLAlchemy models
+
+class BuildResponse(BaseModel):
+    id: int
+    user_id: int
+    vehicle_id: int 
+    nickname: str | None 
+    cover_picture_url: str
+    description: str 
+    vehicle: VehicleResponse
+
+    class Config:
+        orm_mode = True

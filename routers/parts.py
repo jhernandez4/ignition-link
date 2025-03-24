fc1d651035ee69c8ca0965a95038087f17c08d46
jhernandez4 <jokhernandez120744@gmail.com>
2025-03-23 21:38:29 -0700

    feat: query brands by name endpoint

--- routers/parts.py
+++ routers/parts.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter, Depends, HTTPException, status, Query
 from fastapi.responses import JSONResponse
 from typing import Annotated
@@ -41,6 +40,30 @@ def get_brands_list(session: SessionDep):
         .order_by(Brand.name.asc())
     ).all()
 
+    return brands_list
+
+@router.get("/brands/query", response_model=list[Brand])
+def query_brands(
+    brand_name: str,
+    session: SessionDep,
+    offset: int = 0,
+    # Less than or equal to 5; default to 5 
+    limit: Annotated[int, Query(le=5)] = 5,
+):
+    brands_list = session.exec(
+        select(Brand)
+        .where(Brand.name.ilike(f"%{brand_name}%"))
+        .order_by(Brand.name.asc())
+        .offset(offset)
+        .limit(limit)
+    ).all()
+
+    if brands_list is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Failed to retrieve brands. List is null"
+        )
+    
     return brands_list
 
 class CreateNewPartRequest(BaseModel):

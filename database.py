9d9f32a5524306f7670d0e715a50da865a617e07
jhernandez4 <jokhernandez120744@gmail.com>
2025-04-15 15:17:25 -0700

    fix(database.py): include created_at field in like and comment table

--- database.py
+++ database.py
@@ -50,6 +50,7 @@ class Like(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     post_id: int = Field(foreign_key="post.id")
     user_id: int = Field(foreign_key="user.id")
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     user: User = Relationship(back_populates="likes")
     post: Post = Relationship(back_populates="likes")
@@ -59,7 +60,7 @@ class Comment(SQLModel, table=True):
     post_id: int = Field(foreign_key="post.id")
     user_id: int = Field(foreign_key="user.id")
     comment: str
-    created_at: datetime | None = Field(default=None)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     user: User = Relationship(back_populates="comments")
     post: Post = Relationship(back_populates="comments")
